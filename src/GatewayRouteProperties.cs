ac17fe58b450dff9ce4a73ec5815bd242f4240ce
Talen Fisher <talen.fisher@cythral.com>
2021-06-15 21:22:44 -0500

    Remove Validation Attributes

--- src/GatewayRouteProperties.cs
+++ src/GatewayRouteProperties.cs
@@ -1,9 +1,6 @@
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 using Amazon.AppMesh.Model;
-
-using Lambdajection.Attributes;
 
 namespace Cythral.CloudFormation.Resources
 {
@@ -20,28 +17,21 @@ namespace Cythral.CloudFormation.Resources
         /// <summary>
         /// Gets or sets the gateway route name.
         /// </summary>
-        [Required]
-        [UpdateRequiresReplacement]
         public string GatewayRouteName { get; set; } = null!;
 
         /// <summary>
         /// Gets or sets the mesh name.
         /// </summary>
-        [Required]
-        [UpdateRequiresReplacement]
         public string MeshName { get; set; } = null!;
 
         /// <summary>
         /// Gets or sets the mesh owner.
         /// </summary>
-        [Required]
-        [UpdateRequiresReplacement]
         public string MeshOwner { get; set; } = null!;
 
         /// <summary>
         /// Gets or sets the gateway route spec.
         /// </summary>
-        [Required]
         public GatewayRouteSpec Spec { get; set; } = null!;
 
         /// <summary>
@@ -52,8 +42,6 @@ namespace Cythral.CloudFormation.Resources
         /// <summary>
         /// Gets or sets the virtual gateway name.
         /// </summary>
-        [Required]
-        [UpdateRequiresReplacement]
         public string VirtualGatewayName { get; set; } = null!;
     }
 }
